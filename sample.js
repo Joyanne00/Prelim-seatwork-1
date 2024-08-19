5607308cde5243121d9171fff28529adce33c9c8
Joy Anne Lutero <95603-22267140@SERVER2.DOMAIN>
2024-08-19 18:28:18 +0800

    first commit

---
+++ sample.js
@@ -0,0 +1,19 @@
+const user = { 
+    fname: "Joy Anne",
+    lname: "Lutero",
+    gender: "female",
+    age: 23
+}
+
+let value1 = ""
+let value2 = ""
+let value3 = ""
+
+value1 = user.fname
+value2 = user.lname
+value3 = user.age
+
+//I am Joy Anne Lutero and I am 23 yrs old
+
+
+console.log("I am " + value1 + " " + value2 + " and I am " + value3 + " yrs old") 
